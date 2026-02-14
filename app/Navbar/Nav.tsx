4f56958eee16e9ce0a9bc537e35492f856cf4974
RodHdez <mrodrigoh@yahoo.com>
2026-02-14 14:27:54 -0800

    Nav Bar
    Added the NavBar placeholders visually for future testing

--- app/Navbar/Nav.tsx
+++ app/Navbar/Nav.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { GrTechnology } from 'react-icons/gr'
+import { navlinks } from '../constant/constant'
+import Link from 'next/link'
+
 
 const Nav = () => {
   return (
-    <div>
-      nav
+    <div className="transition-all duration-200 h-[12vh] z-[100] fixed w-full">
+      <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
+        {/*LOGO */}
+      <div className='flex items-center space-x-2'>
+        <div className='w-10 h-10 bg-blue-800 rounded-full flex items-center justify-center flex-col'>
+            <GrTechnology className='w-6 h-6 text-white'/>
+        </div>
+      </div>
+      {/*Navelinks */}
+      <div className='hidden lg:flex items-center space-x-10'>
+       {navlinks.map((link)=>{
+            return<Link href={link.url} key={link.id} className='text-white hover:text-rose-500 transition-all duration-200'>
+                <p>{link.label}</p>
+            </Link>
+       })}
+      </div>
+    </div>
     </div>
   )
 }
---
+++ app/constant/constant.ts
@@ -0,0 +1,37 @@
+export const navlinks = [
+    {
+        id:1,
+        url: '#',
+        label: 'Home',
+    },
+    {
+        id:2,
+        url: '#',
+        label: 'Inicio',
+    },
+    {
+        id:3,
+        url: '#',
+        label: 'Perfil',
+    },
+    {
+        id:4,
+        url: '#',
+        label: 'Ranking',
+    },
+    {
+        id:5,
+        url: '#',
+        label: 'Resultados',
+    },
+    {
+        id:6,
+        url: '#',
+        label: 'Streaming',
+    },
+    {
+        id:7,
+        url: '#',
+        label: 'Tienda',
+    }
+]
